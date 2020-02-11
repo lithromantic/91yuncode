4d4cf55828096f3e3c6f6c2b138d351e6d598726
lithromantic <223941@whut.edu.cn>
2020-02-12 02:01:15 +0800

    Update limit_net_kill_ssr.py

--- limit_net_kill_ssr.py
+++ limit_net_kill_ssr.py
@@ -2,7 +2,7 @@
 
 limit_total=0# limit_total 上传+下载的流量限制，单位GB，如果不限制就是0，如果限制1T就是1024
 limit_in=0# limit_in 下载的流量限制，单位GB，如果不限制就是0，如果限制1T就是1024
-limit_out=0# limit_out 上传的流量限制，单位GB，如果不限制就是0，如果限制1T就是1024
+limit_out=1000# limit_out 上传的流量限制，单位GB，如果不限制就是0，如果限制1T就是1024
 sleep=60#多久检查一次，单位是秒
 
 
@@ -20,7 +20,8 @@ while True:
 			NET_OUT=int(mdata[4])/1024
 			break
 
-	killssr="`ps aux|grep server.py|awk '{print \"kill \"$2}'`"
+	#killssr="`ps aux|grep server.py|awk '{print \"kill \"$2}'`"
+	killssr="'curl -d \"text=sgp2 run out of bandwidth\" -X POST https://tgbot.lbyczf.com/sendMessage/cnbe1mjnqnx79p > /dev/null 2>&1'"
 	if (limit_total != 0 and (NET_IN+NET_OUT)>=limit_total):
 		os.system(killssr)
 		break
